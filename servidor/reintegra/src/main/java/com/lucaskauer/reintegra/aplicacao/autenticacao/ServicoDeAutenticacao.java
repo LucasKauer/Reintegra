d2e24839269d07825e54cff1f79617cf6eb07421
Lucas de Freitas Kauer <lucas.kauerrs@hotmail.com>
2017-10-04 22:38:28 -0300

    refactor(exemplo): removendo arquivos da estrutura inicial que foram criados apenas como exemplos.

--- servidor/reintegra/src/main/java/com/lucaskauer/reintegra/aplicacao/autenticacao/ServicoDeAutenticacao.java
+++
@@ -1,8 +0,0 @@
-package com.lucaskauer.reintegra.aplicacao.autenticacao;
-
-import com.lucaskauer.reintegra.aplicacao.autenticacao.models.UsuarioModel;
-import com.lucaskauer.reintegra.aplicacao.autenticacao.request.LoginRequest;
-
-public interface ServicoDeAutenticacao {
-	UsuarioModel login(LoginRequest login) throws Exception;
-}
